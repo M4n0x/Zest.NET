819191a1e2ef70fbae149e1a7b70f2443aa028bc
Lucas Fridez <lfridez@artionet.com>
2021-03-29 17:23:48 +0200

    Add: Api get with Bearer token

--- Zest.Net/Zest.Net.Front/Services/ZestClient.cs
+++ Zest.Net/Zest.Net.Front/Services/ZestClient.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
+using Zest.Net.Front.Extensions;
 
 namespace Zest.Net.Front.Services
 {
@@ -21,6 +23,7 @@ namespace Zest.Net.Front.Services
             object[] forecasts = null;
             try
             {
+                await http.GetJsonAsync<object>(path, new AuthenticationHeaderValue("Bearer", "token"));
                 forecasts = await http.GetFromJsonAsync<object[]>(
                     "WeatherForecast");
             }
