3654861f2ae770aa1e115448fdadcb7fd92d51eb
Lucas Fridez <lfridez@artionet.com>
2021-03-29 17:17:58 +0200

    Add: Default ZestClient

---
+++ Zest.Net/Zest.Net.Front/Services/ZestClient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace Zest.Net.Front.Services
+{
+    public class ZestClient
+    {
+        private readonly HttpClient http;
+
+        public ZestClient(HttpClient http)
+        {
+            this.http = http;
+        }
+
+        public async Task<object[]> GetAsync(string path = "")
+        {
+            object[] forecasts = null;
+            try
+            {
+                forecasts = await http.GetFromJsonAsync<object[]>(
+                    "WeatherForecast");
+            }
+            catch
+            {
+                // TODO
+            }
+
+            return forecasts;
+        }
+    }
+}
