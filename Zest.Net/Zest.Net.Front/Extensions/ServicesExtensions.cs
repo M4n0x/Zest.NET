202ed382b200d9205aa70b37d91f8fdbfc9ce883
Lucas Fridez <lfridez@artionet.com>
2021-03-29 17:21:47 +0200

    Add: Services extensions

---
+++ Zest.Net/Zest.Net.Front/Extensions/ServicesExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text.Json;
+
+namespace Zest.Net.Front.Extensions
+{
+    public static class ServicesExtensions
+    {
+
+        public static async Task<T> GetJsonAsync<T>(this HttpClient httpClient, string url, AuthenticationHeaderValue authorization)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            request.Headers.Authorization = authorization;
+
+            var response = await httpClient.SendAsync(request);
+            var responseBytes = await response.Content.ReadAsByteArrayAsync();
+            return JsonSerializer.Deserialize<T>(responseBytes, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+        }
+    }
+}
