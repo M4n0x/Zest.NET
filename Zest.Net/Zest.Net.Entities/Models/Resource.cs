cf9d9480df1cea9d6a557edebcba3b5a8fd56baf
Lucas Fridez <lfridez@artionet.com>
2021-04-12 17:24:42 +0200

    Add: Resource (empty class) and its repository

---
+++ Zest.Net/Zest.Net.Entities/Models/Resource.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Zest.Net.Entities.Attributes;
+using Zest.Net.Entities.Interfaces;
+
+namespace Zest.Net.Entities.Models
+{
+    /// <summary>
+    /// Resource data class
+    /// </summary>
+    [ApiPath("ressources")]
+    public class Resource
+    {
+        /// <summary>
+        /// Resource Id
+        /// </summary>
+        public int Id { get; set; }
+    }
+}
---
+++ Zest.Net/Zest.Net.Entities/Repositories/ResourcesHttpRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Zest.Net.Entities.Client;
+using Zest.Net.Entities.Interfaces;
+using Zest.Net.Entities.Models;
+
+namespace Zest.Net.Entities.Repositories
+{
+    public class ResourcesHttpRepository : GenericHttpRepository<Resource>
+    {
+        public ResourcesHttpRepository(ZestClient client) : base(client)
+        {
+
+        }
+    }
+}
