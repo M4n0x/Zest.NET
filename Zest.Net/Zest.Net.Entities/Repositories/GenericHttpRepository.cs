1370f922babbe502a900ecf6005b1073606a20c1
Lucas Fridez <lfridez@artionet.com>
2021-04-12 22:25:00 +0200

    Docs: Generic Repository

--- Zest.Net/Zest.Net.Entities/Repositories/GenericHttpRepository.cs
+++ Zest.Net/Zest.Net.Entities/Repositories/GenericHttpRepository.cs
@@ -10,42 +10,86 @@ using Zest.Net.Entities.Models;
 
 namespace Zest.Net.Entities.Repositories
 {
-
+    /// <summary>
+    /// Generic Repository
+    /// </summary>
+    /// <typeparam name="TEntity">Entity type</typeparam>
     public class GenericHttpRepository<TEntity> : IGenericHttpRepository<TEntity> where TEntity : class
     {
+        /// <summary>
+        /// Zest client
+        /// </summary>
         private readonly ZestClient _client;
 
+        /// <summary>
+        /// Api path for used entity (TEntity)
+        /// </summary>
         public string ApiPath => typeof(TEntity).GetCustomAttribute<ApiPathAttribute>().Path;
 
+        /// <summary>
+        /// Generic Repository constructor
+        /// </summary>
+        /// <param name="client">Zest client</param>
         public GenericHttpRepository(ZestClient client)
         {
             _client = client;
         }
 
+        /// <summary>
+        /// Get entities
+        /// </summary>
+        /// <returns>Api response</returns>
         public virtual async Task<IEnumerable<TEntity>> Get()
         {
             return await _client.Get<TEntity>(ApiPath);
         }
 
-        public Task<TEntity> GetById()
+        /// <summary>
+        /// Get entity by id
+        /// </summary>
+        /// <param name="id">Entity id to get</param>
+        /// <returns>Api response</returns>
+        public Task<TEntity> GetById(int id)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Insert entity in database
+        /// </summary>
+        /// <param name="entity">entity to insert</param>
+        /// <returns>Api response</returns>
         public Task Insert(TEntity entity)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Update an entity by id
+        /// </summary>
+        /// <param name="id">Entity id to update</param>
+        /// <param name="entity">Entity data</param>
+        /// <returns>Api response</returns>
         public Task Update(int id, TEntity entity)
         {
             throw new NotImplementedException();
         }
+
+        /// <summary>
+        /// Delete an entity
+        /// </summary>
+        /// <param name="entity">Entity to delete</param>
+        /// <returns>Api response</returns>
         public Task Delete(TEntity entity)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Delete an entity by id
+        /// </summary>
+        /// <param name="id">Entity id to delete</param>
+        /// <returns>Api response</returns>
         public Task Delete(int id)
         {
             throw new NotImplementedException();
