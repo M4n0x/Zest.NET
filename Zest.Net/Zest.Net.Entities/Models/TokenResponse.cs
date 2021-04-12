58eae15fa43e5afa7a34eb4fb8928b89f7bc720a
Lucas Fridez <lfridez@artionet.com>
2021-04-12 22:20:23 +0200

    Docs: Token Response

--- Zest.Net/Zest.Net.Entities/Models/TokenResponse.cs
+++ Zest.Net/Zest.Net.Entities/Models/TokenResponse.cs
@@ -5,10 +5,19 @@ using System.Threading.Tasks;
 
 namespace Zest.Net.Entities.Models
 {
+    /// <summary>
+    /// Token response class
+    /// </summary>
     public class TokenResponse
     {
+        /// <summary>
+        /// Refresh token value
+        /// </summary>
         public string Refresh { get; set; }
 
+        /// <summary>
+        /// Access token value
+        /// </summary>
         public string Access { get; set; }
     }
 }
