3166980d6b6aa4c0d881660e875dd76ab259db4e
Lucas Fridez <lfridez@artionet.com>
2021-04-12 22:19:48 +0200

    Docs: User http repository

--- Zest.Net/Zest.Net.Entities/Repositories/UserHttpRepository.cs
+++ Zest.Net/Zest.Net.Entities/Repositories/UserHttpRepository.cs
@@ -8,11 +8,18 @@ using Zest.Net.Entities.Models;
 
 namespace Zest.Net.Entities.Repositories
 {
+    /// <summary>
+    /// User Http repository
+    /// </summary>
     public class UserHttpRepository : GenericHttpRepository<User>
     {
+        /// <summary>
+        /// User Http Repository constructor
+        /// </summary>
+        /// <param name="client">Zest client</param>
         public UserHttpRepository(ZestClient client) : base(client)
         {
-
+            // Nothing
         }
     }
 }
