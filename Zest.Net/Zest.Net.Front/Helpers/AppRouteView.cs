59c2d1ee0833c20a3fca75c3e774c761f4b93de6
Lucas Fridez <lfridez@artionet.com>
2021-04-24 11:35:35 +0200

    Add: AppRouteVIew helper to check user authentication status

---
+++ Zest.Net/Zest.Net.Front/Helpers/AppRouteView.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Zest.Net.Entities.Client;
+
+namespace Zest.Net.Front.Helpers
+{
+    public class AppRouteView : RouteView
+    {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        [Inject]
+        public ZestClient Client { get; set; }
+
+        protected override void Render(RenderTreeBuilder builder)
+        {
+            Console.WriteLine("OK");
+            var authorize = Attribute.GetCustomAttribute(RouteData.PageType, typeof(AuthorizeAttribute)) != null;
+            if (authorize && Client.Token != null)
+            {
+                NavigationManager.NavigateTo($"login");
+            }
+            else
+            {
+                base.Render(builder);
+            }
+        }
+    }
+}
