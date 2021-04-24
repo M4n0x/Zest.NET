5eaf0283cea77ee5d0dade2ac39ae41c15bc629c
Lucas Fridez <lfridez@artionet.com>
2021-04-24 16:22:24 +0200

    Add: Logout methods in Client and repository

---
+++ Zest.Net/Zest.Net.Front/Pages/Logout.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Zest.Net.Entities.Repositories;
+
+namespace Zest.Net.Front.Pages
+{
+    public partial class Logout
+    {
+        /// <summary>
+        /// Authentication repository
+        /// </summary>
+        [Inject]
+        public AuthHttpRepository AuthRepository { get; set; }
+
+        protected override void OnInitialized()
+        {
+            AuthRepository.Logout();
+        }
+    }
+}
